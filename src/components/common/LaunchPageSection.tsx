63c6ac1cad12fdbf196e4e8cc3cdf60c2d3b28be
manucreative <manwiks2@gmail.com>
2025-09-03 12:52:54 +0300

    updated launcher

--- src/components/common/LaunchPageSection.tsx
+++ src/components/common/LaunchPageSection.tsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 import { Typewriter } from 'react-simple-typewriter';
 import { useConfig } from "../../context/ConfigProvider";
+import { Link } from "react-router";
 const LaunchPageSection: React.FC = () =>{
   const { configurations } = useConfig();
   const typeData = configurations?.["home typewriter"] ? configurations?.["home typewriter"].split(",") : [];
@@ -11,62 +12,71 @@ const LaunchPageSection: React.FC = () =>{
         <div className="container mx-auto px-4 relative z-10">
     <div className="flex justify-center">
       <div className="p-6 text-center">
-        <motion.h1 
-        initial={{ opacity: 0.3, scale: 0.5 }}
-        whileInView={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.8 }}
-        className="neon-text-2 text-4xl sm:text-5xl font-bold bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text mb-10">
-          {configurations?.["home typewriter"] && (
-          <Typewriter
-              words={
-                typeData
-              }
-              loop={true}
-              cursor
-              cursorStyle="|"
-              typeSpeed={80}
-              deleteSpeed={50}
-              delaySpeed={1500}
-              />
-            )}
-        </motion.h1>
 
-        <motion.img 
-          initial={{ opacity: 0, scale: 0.5 }}
+          {configurations?.["home show-typewriter"] === 'yes' && (
+          <motion.h1 
+          initial={{ opacity: 0.3, scale: 0.5 }}
           whileInView={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 2 }}
-          className="mb-5 w-full sm:w-auto mx-auto" 
-          src={`${configUrl}/${configurations?.['advert banner']}`} 
-          alt="Vector Design" 
-        />
+          transition={{ duration: 0.8 }}
+          className="neon-text-2 text-4xl sm:text-5xl font-bold bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text mb-10">
+            {configurations?.["home typewriter"] && (
+            <Typewriter
+                words={
+                  typeData
+                }
+                loop={true}
+                cursor
+                cursorStyle="|"
+                typeSpeed={80}
+                deleteSpeed={50}
+                delaySpeed={1500}
+                />
+              )}
+          </motion.h1>
+          )}
+      {configurations?.["home show-advert-banner"] === 'yes' && (
+        <>
+            <motion.img 
+              initial={{ opacity: 0, scale: 0.5 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 2 }}
+              className="mb-5 w-full sm:w-auto mx-auto" 
+              src={`${configUrl}/${configurations?.['advert banner']}`} 
+              alt="Vector Design" 
+            />
 
-        <div className="w-full overflow-hidden flex items-center">
-          <motion.span 
-          className="px-3 bg-gradient-to-r from-green-400 to-pink-400 text-transparent bg-clip-text text-2xl justify-center"
-          animate={{
-            x: ["100%", "10%", "0%","-100%"],
-            // opacity: [0, 1, 1, 0],
-          }}
-          transition={{
-            times: [0, 0.2, 0.7, 1],
-            duration: 4,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-          >
-            {configurations?.['slide text']}
-          </motion.span>
-        </div>
-
-        {/* Neon Button */}
-        <button className="neon-button relative px-6 py-3 text-lg font-bold uppercase tracking-wider text-white bg-transparent rounded-full overflow-hidden transition-transform duration-300 hover:scale-105 shadow-lg mt-6">
-          Learn More
-        </button>
+            <div className="w-full overflow-hidden flex items-center">
+              <motion.span 
+              className="px-3 bg-gradient-to-r from-green-400 to-pink-400 text-transparent bg-clip-text text-2xl justify-center"
+              animate={{
+                x: ["100%", "10%", "0%","-100%"],
+                // opacity: [0, 1, 1, 0],
+              }}
+              transition={{
+                times: [0, 0.2, 0.7, 1],
+                duration: 4,
+                repeat: Infinity,
+                ease: "easeInOut",
+              }}
+              >
+                {configurations?.['slide text']}
+              </motion.span>
+            </div>
+            </>
+          )}
+          {configurations?.["home show-learn-btn"] === 'yes' && (
+            <Link 
+                to={configurations?.['home btn-url']}
+                className="neon-button relative px-6 py-3 text-lg font-bold uppercase tracking-wider text-white bg-transparent rounded-full overflow-hidden transition-transform duration-300 hover:scale-105 shadow-lg mt-6"
+              >
+                {configurations?.['home btn-text']}
+              </Link>
+          )}
       </div>
     </div>
-
-    <hr className="border-none h-[4px] w-full rounded bg-gradient-to-r from-purple-700 via-green-500 to-pink-500 shadow-[0_0_10px_purple,0_0_20px_rgb(241,17,129),0_0_30px_rgb(1,134,1)]" />
-
+          {configurations?.["home show-hr"] === 'yes' && (
+            <hr className="border-none h-[4px] w-full rounded bg-gradient-to-r from-purple-700 via-green-500 to-pink-500 shadow-[0_0_10px_purple,0_0_20px_rgb(241,17,129),0_0_30px_rgb(1,134,1)]" />
+          )}
   </div>
     )
 }
--- src/components/emndex/Home.tsx
+++ src/components/emndex/Home.tsx
@@ -86,7 +86,7 @@ function Home(){
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.8, delay: 0.5 }}
                 className="lg:w-1/2 px-4 mt-6 lg:mt-0">
-              <AboutusContent bg={`url('${configUrl}/${configurations?.['home banner']}')`} content={limitedContent} classData="bg-gradient-to-l from-[#7FDBFF] to-[#00f514] bg-clip-text text-transparent font-bold">
+              <AboutusContent bg={`url('${configUrl}/${configurations?.['about-bg']}')`} content={limitedContent} classData="bg-gradient-to-l from-[#7FDBFF] to-[#00f514] bg-clip-text text-transparent font-bold">
                 <button className="z-10 neon-button relative px-6 py-3 text-sm font-bold uppercase tracking-wider text-white bg-transparent rounded-full overflow-hidden transition-transform duration-300 hover:scale-105 mt-6 focus:outline-none"
                 onClick={()=> navigate("/home/about")}>
                     Learn More
